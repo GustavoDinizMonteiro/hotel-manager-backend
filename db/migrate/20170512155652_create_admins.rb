70f2642750edf79d8ea03d6b3a9a1b667c4fd292
Gustavo Diniz Monteiro <gustavo.monteiro@ccc.ufcg.edu.br>
2017-05-12 13:03:30 -0300

    admin model created

---
+++ db/migrate/20170512155652_create_admins.rb
@@ -0,0 +1,12 @@
+class CreateAdmins < ActiveRecord::Migration[5.1]
+  def change
+    create_table :admins do |t|
+      t.string :name, null: false
+      t.string :username, null: false, unique: true
+      t.string :password, null: false
+      t.boolean :superuser, null: false, default: false
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170512153213) do
+ActiveRecord::Schema.define(version: 20170512155652) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "admins", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "username", null: false
+    t.string "password", null: false
+    t.boolean "superuser", default: false, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "apartaments", force: :cascade do |t|
     t.string "number", null: false
